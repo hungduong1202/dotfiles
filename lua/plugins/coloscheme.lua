54508d8ebd7156cce2994fc783e8cf6ecf3b704a
Hung Duong <hungtd12384@gmail.com>
2024-12-31 01:23:46 +0700

    init git

---
+++ lua/plugins/coloscheme.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/tokyonight.nvim",
+  priority = 1000,
+  opts = {
+    transparent = true,
+    style = "night",
+    styles = { sidebars = "transparent", floats = "transparent", comments = { italic = true }, keywords = { italic = true } }
+  },
+
+}
